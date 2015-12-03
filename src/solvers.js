7546523888b07630ec4b5ac0ff4acd42b4f670e9
puzzlehe4d <davoodharun@gmail.com>
2015-12-02 20:09:46 -0800

    Created shell for countRooks.

--- src/solvers.js
+++ src/solvers.js
@@ -30,7 +30,7 @@ window.findNRooksSolution = function(n) {
   }
   
   var solution = newBoard.rows();
-  console.log(solution);
+  
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -52,40 +52,53 @@ window.countNRooksSolutions = function(n) {
 //   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
 //   return solutionCount;
 
-var board = new Board({n:n});
-board.togglePiece(0,1);
-
  var solution = 0; //fixme
 
-  var countSolutions(oldBoard) {
-    var boardCopy = new (oldBoard.rows);
-    //runs conflict on each row and column (for queens cell) and build up an array of spaces we can toggle
-    for (var i = 0 ; i < this.rows.length; i++) {
-      for (var j = 1; j < this.rows.length, j++) {
-        boardCopy.togglePiece(i,j);
-        rowConflict = !boardCopy.hasRowConflictAt(i);
-        colConflict = !boardCopy.hasColConflictAt(j);
-        if (rowConflict && colConflict) {
-          //invoke countSoultions()
-        }
-        else {
-          oldBoard.toggle(i,j)
-        }
 
+  for (var i = 0 ; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+
+      var board = new Board({n:n});
+      console.log("board", board.rows());
+      board.togglePiece(i,j);
+
+  var countSolutions = function(oldBoard) {
+    console.log("oldboard", oldBoard.rows());
+    var numberOfPieces = _.reduce((_.flatten(oldBoard.rows())), function(sum, item) { return sum + item;});
+    console.log(numberOfPieces);
+    if (numberOfPieces >= n) {
+      
+      console.log(oldBoard.rows())
+      solution++;
+      return;
+    }
+
+      var boardCopy = new Board(oldBoard.rows());
+      //runs conflict on each row and column (for queens cell) and build up an array of spaces we can toggle
+      for (var i = 0 ; i < boardCopy.rows.length; i++) {
+        for (var j = 0; j < boardCopy.rows.length; j++) {
+          if (boardCopy.get(i, j) === 0) {
+            boardCopy.togglePiece(i,j);
+            rowConflict = !boardCopy.hasRowConflictAt(i);
+            colConflict = !boardCopy.hasColConflictAt(j);
+            if (rowConflict && colConflict) {
+              countSolutions(boardCopy);
+            }
+            else {
+              boardCopy.toggle(i,j);
+            }
+          }
+
+          }
       }
     }
-    if open space array.lenth is 0
-      if n pieces present
-        solution ++
-    else if array.length > 0   
-      for each open space 
-        counSolutions(new Board([]))
-    }
+    countSolutions(board);
+  }
+}
 
-    counsSolutions(board);
+    
 
     return solution;
-
 
 };
 
@@ -100,7 +113,7 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
  
-  }
+  
 
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
