6b83c700b8ff874d7c0ce6de3da6727e0ffc5ec8
puzzlehe4d <davoodharun@gmail.com>
2015-12-03 12:24:54 -0800

    Getting closer

--- src/solvers.js
+++ src/solvers.js
@@ -38,7 +38,9 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-//   var recurse = function(n){
+  //debugger;
+
+// if  var recurse = function(n){
 //     if(n==2){
 //       return 1;
 //     }
@@ -55,45 +57,48 @@ window.countNRooksSolutions = function(n) {
  var solution = 0; //fixme
 
 
-  for (var i = 0 ; i < n; i++) {
-    for (var j = 0; j < n; j++) {
+  for (var i = 0; i < n; i++) {
 
       var board = new Board({n:n});
-      console.log("board", board.rows());
-      board.togglePiece(i,j);
+      
+      board.togglePiece(i,0);
 
   var countSolutions = function(oldBoard) {
-    console.log("oldboard", oldBoard.rows());
+ 
     var numberOfPieces = _.reduce((_.flatten(oldBoard.rows())), function(sum, item) { return sum + item;});
-    console.log(numberOfPieces);
+
     if (numberOfPieces >= n) {
       
-      console.log(oldBoard.rows())
+      console.log("solution", oldBoard.rows())
       solution++;
+
       return;
     }
 
       var boardCopy = new Board(oldBoard.rows());
       //runs conflict on each row and column (for queens cell) and build up an array of spaces we can toggle
-      for (var i = 0 ; i < boardCopy.rows.length; i++) {
-        for (var j = 0; j < boardCopy.rows.length; j++) {
-          if (boardCopy.get(i, j) === 0) {
-            boardCopy.togglePiece(i,j);
-            rowConflict = !boardCopy.hasRowConflictAt(i);
-            colConflict = !boardCopy.hasColConflictAt(j);
+      for (var k = 0 ; k < boardCopy.rows().length; k++) {
+        for (var l = 0; l < boardCopy.rows().length; l++) {
+          if (boardCopy.get(k)[l] === 0) {
+            boardCopy.togglePiece(k,l);
+            rowConflict = !boardCopy.hasRowConflictAt(k);
+            colConflict = !boardCopy.hasColConflictAt(l);
             if (rowConflict && colConflict) {
               countSolutions(boardCopy);
+              boardCopy.togglePiece(k,l);
+              
             }
             else {
-              boardCopy.toggle(i,j);
+              boardCopy.togglePiece(k,l);
             }
           }
 
           }
       }
     }
+
+    //debugger;
     countSolutions(board);
-  }
 }
 
     
